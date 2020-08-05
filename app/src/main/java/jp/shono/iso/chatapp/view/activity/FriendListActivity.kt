0dcd1d382aaeb974a794e13b38a446b77550469d
磯 翔野 <shono-iso@united.jp>
2020-08-05 12:31:12 +0900

    名前の表示まで至ったものの処理が気にくわない

--- app/src/main/java/jp/shono/iso/chatapp/view/activity/FriendListActivity.kt
+++ app/src/main/java/jp/shono/iso/chatapp/view/activity/FriendListActivity.kt
@@ -1,38 +1,21 @@
 package jp.shono.iso.chatapp.view.activity
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.content.ContentValues
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Color
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
 import jp.shono.iso.chatapp.R
 import jp.shono.iso.chatapp.databinding.ActivityChatRoomBinding
-import jp.shono.iso.chatapp.model.chatMessage
-import jp.shono.iso.chatapp.model.userData
-import jp.shono.iso.chatapp.viewmodel.ChatRoomViewModel
 import jp.shono.iso.chatapp.viewmodel.FriendListViewModel
-import kotlinx.android.synthetic.main.activity_chat_room.*
-import java.text.SimpleDateFormat
-import java.util.*
+import kotlinx.android.synthetic.main.activity_friend_list.*
 
 class FriendListActivity : AppCompatActivity() {
 
@@ -47,26 +30,21 @@ class FriendListActivity : AppCompatActivity() {
     }
 
     fun initialize() {
-        initBinding()
         initViewModel()
         initLayout()
     }
 
-    private fun initBinding() {
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_friend_list)
-        binding.lifecycleOwner = this
-    }
-
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(this).get(FriendListViewModel::class.java).apply {
-            /*
-            initSetting()
-            binding.editMessage = editMessage
-            chatMessageList.observe(this@FriendListActivity, androidx.lifecycle.Observer {
+            friendList.observe(this@FriendListActivity, androidx.lifecycle.Observer {
+                Log.d("testinfo", "ここまで動いてる1")
+                loadName()
+            })
+            nameList.observe(this@FriendListActivity, androidx.lifecycle.Observer {
+                Log.d("testinfo", "ここまで動いてる2" + it.size)
                 customAdapter.refresh(it)
             })
-
-             */
+            initFriendList()
         }
     }
 
@@ -75,7 +53,7 @@ class FriendListActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        chatMessageRecyclerView.apply {
+        friendRecyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(this@FriendListActivity)
             adapter = customAdapter
@@ -89,12 +67,9 @@ class FriendListActivity : AppCompatActivity() {
                     val totalItemCount = layoutManager.itemCount
                     val pastVisibleItems = layoutManager.findFirstVisibleItemPosition()
                     if (pastVisibleItems + visibleItemCount >= totalItemCount) {
-                        /*
-                        viewModel.chatRoomList.value?.also {
-                            viewModel.loadChatRoom(it.last().datetime)
+                        viewModel.friendList.value?.also {
+                            viewModel.loadFriendList(it.last().datetime)
                         }
-                        
-                         */
                     }
                 }
             })
@@ -103,15 +78,15 @@ class FriendListActivity : AppCompatActivity() {
 }
 
 class FriendListRecyclerViewAdapter(val context: Context) : RecyclerView.Adapter<FriendListViewHolder>() {
-    var friendList = mutableListOf<userData>()
-
-    fun refresh(newFriendList: MutableList<userData>?) {
+    var nameList = mutableListOf<String>()
+    fun refresh(newFriendList: MutableList<String>?) {
         newFriendList?.also {
-            friendList.apply {
+            nameList.apply {
                 clear()
                 addAll(it)
             }
             notifyDataSetChanged()
+            Log.d("testinfo", "ここまで動いてる3" + nameList.size)
         }
     }
 
@@ -122,16 +97,16 @@ class FriendListRecyclerViewAdapter(val context: Context) : RecyclerView.Adapter
         return FriendListViewHolder(inflate)
     }
 
-    @SuppressLint("ResourceAsColor")
     override fun onBindViewHolder(holder: FriendListViewHolder, position: Int) {
-        val item = friendList.get(position)
+        val item = nameList.get(position)
         holder.apply {
-            userNameView.setText(item.name)
+            Log.d("testinfo", item)
+            userNameView.setText(item)
         }
     }
 
     override fun getItemCount(): Int {
-        return friendList.size
+        return nameList.size
     }
 }
 
--- app/src/main/java/jp/shono/iso/chatapp/viewmodel/FriendListViewModel.kt
+++ app/src/main/java/jp/shono/iso/chatapp/viewmodel/FriendListViewModel.kt
@@ -1,8 +1,93 @@
 package jp.shono.iso.chatapp.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import jp.shono.iso.chatapp.model.friend
 
 class FriendListViewModel(application: Application) : AndroidViewModel(application) {
-    
+    val friendList = MutableLiveData<MutableList<friend>>()
+    val uidList = mutableListOf<String>()
+    val nameList = MutableLiveData<MutableList<String>>()
+    lateinit var db: FirebaseFirestore
+    var isFirstLoad = true
+    var isFullloaded = false
+    var uid = ""
+
+    fun initFriendList() {
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.also {
+            uid = it.uid
+        }
+        db = FirebaseFirestore.getInstance()
+        db.collection("users")
+            .document(uid)
+            .collection("friends")
+            .orderBy("datetime", Query.Direction.DESCENDING)
+            .limit(10)
+            .get()
+            .addOnSuccessListener { snapshot ->
+                val newUidList = mutableListOf<String>()
+                val newFriendList = mutableListOf<friend>()
+                snapshot.documents.forEach { document ->
+                    val friend = friend(document.get("datetime").toString().toLong())
+                    newUidList.add(document.id)
+                    newFriendList.add(friend)
+                }
+                isFirstLoad = false
+                uidList.addAll(newUidList)
+                friendList.postValue(newFriendList)
+            }
+    }
+
+    fun loadName() {
+        val newNameList = mutableListOf<String>()
+        nameList.value?.also {
+            newNameList.addAll(it)
+        }
+        uidList.forEach {
+            db.collection("users")
+                .document(it)
+                .get()
+                .addOnSuccessListener {
+                    newNameList.add(it.data?.get("name").toString())
+                    Log.d("testinfo", "さて" + it.data?.get("name").toString())
+                    nameList.postValue(newNameList)
+                }
+        }
+    }
+
+    fun loadFriendList(datetime: Long) {
+        if (isFirstLoad || isFullloaded) {
+            return
+        }
+        db.collection("users")
+            .document(uid)
+            .collection("friends")
+            .whereLessThan("datetime", datetime)
+            .orderBy("datetime", Query.Direction.DESCENDING)
+            .limit(10)
+            .get()
+            .addOnSuccessListener { snapshot ->
+                if (snapshot.documents.size.equals(0)) {
+                    isFullloaded = true
+                    return@addOnSuccessListener
+                }
+                val newFriendList = mutableListOf<friend>()
+                val newUidList = mutableListOf<String>()
+                friendList.value?.also {
+                    newFriendList.addAll(it)
+                }
+                snapshot.documents.forEach { document ->
+                    val friend = friend(document.get("datetime").toString().toLong())
+                    newUidList.add(document.id)
+                    newFriendList.add(friend)
+                }
+                friendList.postValue(newFriendList)
+            }
+    }
 }
